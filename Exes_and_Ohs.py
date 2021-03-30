df5937b612eb679fe2f7671553b182f92633209d
vladimir <grun_gespenst@tut.by>
2021-03-30 21:25:22 +0300

    o_x

---
+++ Exes_and_Ohs.py
@@ -0,0 +1,14 @@
+# Check to see if a string has the same amount of 'x's and 'o's.
+# The method must return a boolean and be case insensitive.
+# The string can contain any char.
+#Проверьте, есть ли в строке одинаковое количество символов «x» и «o».
+# Метод должен возвращать логическое значение и не учитывать регистр.
+# Строка может содержать любой символ.
+# Examples input/output:
+#
+# XO("ooxx") => true
+# XO("xooxx") => false
+# XO("ooxXm") => true
+# XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+# XO("zzoo") => false
+
